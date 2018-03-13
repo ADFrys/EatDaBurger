8839abe254f9e4403aefe342ec9e27a53ae59360
AngelaF <ADFrys@users.noreply.github.com>
2018-03-13 13:00:05 -0500

    Add first route.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,16 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function(req,res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
 
 
 
